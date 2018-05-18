f5e25095f277dec203bc4ed249eceba0d185f0d7
yoneyama <yoneyama@wize-net.co.jp>
2018-05-18 20:26:21 +0900

    【完了】liを個別に消す

--- js/text.js
+++ js/text.js
@@ -1,43 +1,60 @@
 (function(){
    $("img").hide(); //チェックボタン画像非表示
-   $("#deletebtn").hide();
+   $(".deletebtn").hide();
 
-
-   $("#allCerect").click(function(){
-     $('img').toggle();
-     $("#list").toggleClass('checked');
+   $("input").on('keypress', function(e){ //liしか追加されないので、liの中身ごと追加できているが、classとidが付けられてない
+     if(e.which === 13){
+       var liDom = $("<li>",{
+         "class" : "list"
+       });
+       $("ul").append($('<li/>').append('<button/>').append('<img>').append('<div/>').append('<input/>'));
+     }
    });
 
-  $("#checkbtn").click(function(){
+   $("#allCerect").on('click', function(){
+     $('img').toggle();
+     $(".task").toggleClass('checked');
+   });
+
+  $(".checkbtn").on('click', function(){　//チェックボタンと色変更・横線の表示非表示切り替え　※要素の取得必要
     $('img').toggle();
-    $("#list").toggleClass('checked');
+    $(".task").toggleClass('checked');
   });
 
-  $("li").hover(function(){
-    $("#deletebtn").show() .css('color',  '#cc9a9a');
+  $("li").hover(function(){ //liにカーソルが来たらdeletebtnを表示する　※liの要素取得必要
+    　var i = $("li").index();
+    $(".deletebtn").show() .css('color',  '#cc9a9a');
   },
   function(){
-    $("#deletebtn").hide();
+    $(".deletebtn").hide();
   }
     );
 
-  $("#deletebtn").hover(function(){
+  $(".deletebtn").hover(function(){ //deletebtnにカーソルが来たら色を濃くする ※liの要素取得必要
     $(this).css('color',  '#af5b5e');
   },
   function(){
     $(this).css('color',  '');
   });
 
-  $("#deletebtn").on('click', function(){
+  $(".deletebtn").on('click', function(){　//deletebtnをクリックしたらliごと削除　
     $(this).parent("li").remove();
   });
 
-  $(".clearComp").hover(function(){ //これをアンダーラインのhoverにする
-    $(this).css('color', 'pink');
+  $(".clearComp").hover(function(){
+    $(this).css('border-bottom', '#777 1px solid');
   },
   function(){
-    $(this).css('color', 'yellow');
-  }
-    );
+    $(this).css('border', '');
+  });
+
+  $(".subcnt li").on('click', function(){
+    $("li").hide();
+    $('list li[class="checked"]').show();
+  });
+
+    $(".list").hover(function(){ //全てのliのインデックス番号が表示される 反応しなかったの同じidを使ってるから？修正してみる
+    console.log($("li").index(this));
+  });
 
 })();
