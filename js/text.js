90d95f24150ad2db04128c30d9ae6c9380b544b9
yoneyama <yoneyama@wize-net.co.jp>
2018-05-22 18:36:47 +0900

    js未確認/scssの変数途中、未整理

--- js/text.js
+++ js/text.js
@@ -5,7 +5,7 @@
    */
   function addNewTask(text) {
     $("#mainlist").append($('<li class="list"/>')
-    .append('<button class="checkbtn"><img src="images/checkicon_02.png">')
+    .append('<button class="checkbtn"><img src="images/checkicon_02.png class="checkicon">')
     .append('<div class="task">' + text + '</div>')
     .append('<input type="button" class="deletebtn" value="×"/>'));
   }
@@ -103,5 +103,6 @@
   $(window).on('load', function () {
     setActiveTaskNumText();
   });
+
 
 })();
