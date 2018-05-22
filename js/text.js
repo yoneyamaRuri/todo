11ddb85dae5c5e21c150f4ae65e00b88ddd28ba1
yoneyama <yoneyama@wize-net.co.jp>
2018-05-22 15:58:23 +0900

    安原さんに修正して頂いた

--- js/text.js
+++ js/text.js
@@ -1,77 +1,107 @@
 (function(){
 
-  $("img").hide(); //チェックボタン画像非表示
-  $(".deletebtn").hide();
-  $(".checkedLine").hide();
-
-
-  $("input").on('keypress', function(e){ //完璧
-  if(e.which === 13){
-    var inputstr = $('.form-control').val();
+  /**
+   * taskの追加
+   */
+  function addNewTask(text) {
     $("#mainlist").append($('<li class="list"/>')
     .append('<button class="checkbtn"><img src="images/checkicon_02.png">')
-    .append('<div class="task">' + inputstr + '</div>')
+    .append('<div class="task">' + text + '</div>')
     .append('<input type="button" class="deletebtn" value="×"/>'));
-    $(".form-control").val("");
-    $("img").hide();
-    $(".deletebtn").hide();
   }
-});
-  $("#allCerect").on('click', function() { //item数が減らない
-    $('img').toggle();
-    $(".task").toggleClass('checkedLine');
 
+  /**
+   * 未完了のtask件数を表示
+   */
+  function setActiveTaskNumText() {
+    var classLength = $('.list:not(.checked)').length;
+    $("#count").text(classLength + " items left");
+  }
+
+
+  // taskリストのul
+  var $taskListContainer = $("ul#mainlist");
+  // task入力欄
+  var $taskInput = $('#input-group input');
+
+
+
+  /**
+   * 新規登録
+   */
+  $taskInput.on('keypress', function(e){ //完璧
+    if(e.which === 13){
+      var inputStr = $('.form-control').val();
+      $(".form-control").val("");
+      addNewTask(inputStr);
+    }
   });
 
-  $("ul").on('click',"li", function() {　
-  var indexNom = $("li").index($(this));
-  $("img").eq(indexNom).toggle();
-  $(this).toggleClass('checked');
-  $(this).toggleClass('checkedLine');　　//これが文字にかからないでliにかかってる
-});
-
-  $("ul").on('mouseenter', "li", function() {
-  　var indexNom = $("li").index($(this));
-  $(".deletebtn").eq(indexNom).show() .css('color',  '#cc9a9a');
-  });
-  $("ul").on('mouseleave', "li", function(){
-  var indexNom = $("li").index($(this));
-  $(".deletebtn").eq(indexNom).hide();
+  /**
+   * 個別taskチェック・チェック解除
+   */
+  $taskListContainer.on('click',"li", function() {
+    $(this).toggleClass('checked');
   });
 
-  $("ul").on('mouseenter', 'input', function() {
-  var indexNom = $("input").index($(this));
-  $("input").eq(indexNom).css('color',  '#af5b5e');
-  });
-  $("ul").on('mouseleave', 'input', function() {
-  var indexNom = $("input").index($(this));
-  $("input").eq(indexNom).css('color',  '');
+  /**
+   * taskの一括チェック・チェック解除
+   */
+  $("#allCerect").on('click', function() {
+    var checkedNum = $('.list:not(.checked)').length;
+    if (checkedNum === 0) {
+      $('.list').removeClass('checked');
+    } else {
+      $('.list:not(.checked)').addClass('checked');
+    }
   });
 
-  $("ul").on('click','.deletebtn', function(){　//deletebtnをクリックしたらliごと削除されるが、なぜか一番下のimgが表示される　
-  $(this).parent("li").remove();
+  /**
+   * 個別task削除
+   */
+  $taskListContainer.on('click','.deletebtn', function(){　//deletebtnをクリックしたらliごと削除されるが、なぜか一番下のimgが表示される　
+    $(this).parent("li").remove();
   });
 
-  $("#clearComp").on({
-  'mouseenter' :function(){
-  $(this).css('border-bottom', '#777 1px solid');
-},
-'mouseleave' :function(){
-  $(this).css('border', '');
-}
-});
-
+  /**
+   * チェック済みを一括削除
+   */
   $(".btn").on('click','#clearComp', function(){
     $('.checked').remove();
   });
 
-  $(function(){
-  $(document).on('click', function() { //ローカル関数にして、liクリック毎に処理を行う
-    var classLength = $('.list').not('.checked').length;
-    $("#count").text(classLength + " items left");
+  /**
+   * フィルタリング：全表示
+   */
+  $('.all').on('click', function() {
+    $taskListContainer.find('li').show();
   });
-});
+
+  /**
+   * フィルタリング：未完了のみ表示
+   */
+  $('.active').on('click', function() {
+    $taskListContainer.find('li').show();
+    $taskListContainer.find('li.checked').hide();
+  });
+
+  /**
+   * フィルタリング：未完了のみ表示
+   */
+  $('.completed').on('click', function() {
+    $taskListContainer.find('li').show();
+    $taskListContainer.find('li:not(.checked)').hide();
+  });
 
 
+  /**
+   * 未完了のtask件数を表示
+   */
+  $(document).on('click keypress', function() {
+    setActiveTaskNumText();
+  });
+  $(window).on('load', function () {
+    setActiveTaskNumText();
+  });
 
 })();
